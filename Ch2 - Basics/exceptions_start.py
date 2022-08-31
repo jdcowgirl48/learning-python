14b37c8cc47f3836c8bceb213fe4181f93bea73a
jdcowgirl48 <jdcowgirl48@gmail.com>
2022-08-31 16:47:29 -0400

    Try error example

--- Ch2 - Basics/exceptions_start.py
+++ Ch2 - Basics/exceptions_start.py
@@ -5,7 +5,7 @@
 
 # Errors can happen in programs, and we need a clean way to handle them
 # TODO: This code will cause an error because you can't divide by zero:
-
+x = 10 / 0
 # TODO: Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
 
