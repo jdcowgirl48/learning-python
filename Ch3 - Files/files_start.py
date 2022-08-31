5363cdd366fe4fb1a3c26182ff866750bdb4c0f4
jdcowgirl48 <jdcowgirl48@gmail.com>
2022-08-31 17:26:27 -0400

    Append textfile.txt

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -6,15 +6,15 @@
 
 def main():  
     # Open a file for writing and create it if it doesn't exist
-    myfile = open("textfile.txt", "w+")
+    # myfile = open("textfile.txt", "w+")
 
     
     # Open the file for appending text to the end
-
+    myfile = open("textfile.txt", "a+")
 
     # write some lines of data to the file
     for i in range(10):
-        myfile.write("This is some text\n")
+        myfile.write("This is some new text\n")
     
     # close the file when done
     myfile.close()
