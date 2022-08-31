433b03883aeea775877d6383b1abc6ddeb3311d6
jdcowgirl48 <jdcowgirl48@gmail.com>
2022-08-31 16:44:43 -0400

    Import math module

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,10 +3,10 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print("The square root of 16 is", math.sqrt(16))
 
 # TODO: in addition to functions, some modules contain useful constants 
 
